51ea0e4a167d698d7d8abe7707b4a76c1f83cd4c
andreruizrt <andrelucasruizalmeida@gmail.com>
2023-12-10 21:58:07 -0300

    merge Refatoração | Code Smell

--- src/main/java/com/sri/jfreecell/CardPileBulk.java
+++ src/main/java/com/sri/jfreecell/CardPileBulk.java
@@ -4,13 +4,31 @@ package com.sri.jfreecell;
  * @author Sateesh Gampala
  *
  */
-public class CardPileBulk extends CardPile {
-   private static final long serialVersionUID = -2102907183340211155L;
-   public CardPile pile;
-   public int count;
 
-   public CardPileBulk(CardPile pile, int count) {
-      this.pile = pile;
-      this.count = count;
-   }
+public class CardPileBulk extends CardPile {
+
+    private static final long serialVersionUID = -2102907183340211155L;
+    private CardPile pile;
+    private int count;
+
+    public CardPileBulk(CardPile pile, int count) {
+        this.pile = pile;
+        this.count = count;
+    }
+
+    public CardPile getCardPile() {
+        return pile;
+    }
+
+    public void setPile(CardPile pile) {
+        this.pile = pile;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
